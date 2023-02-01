e858b1896086dbfb42cc743fa1d6bad73865d224
Dhanalakshmimurugan <98573100+Dhanalakshmimurugan@users.noreply.github.com>
2023-02-01 12:45:56 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+package week2.day2;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int i,j=1;
+		for(i=1;i<=5;i++) {
+			j=j*i;
+		}
+		System.out.println("factorical of "+ 5 +" is "+j);
+	}
+
+}
+ 
---
+++ PrimeNumber.java
@@ -0,0 +1,20 @@
+package week2.day2;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		for(int a=1;a<=20;a++) {
+	
+	 
+			if(a% 2!=0) {
+				System.out.println( a +" is a prime number ");
+				
+				
+			}
+		}
+	}
+}
+
+			
+
+
+
